11f949bb581b161e685b81e533bc7e2e3f89ddcb
Md Danish Alam <danishalam002@gmail.com>
2022-11-25 22:13:52 +0530

    News Ways Day 1

---
+++ config.php
@@ -0,0 +1,39 @@
+<?php
+    $con = mysqli_connect("localhost","root","","news");
+    if($con){
+        echo "ho gya", "<br/>";
+    }
+
+
+    $query = mysqli_query($con,"select * from category");
+
+    while($row = mysqli_fetch_array($query)){
+        echo $row['cat_id'] , "<br/>";
+        echo $row['title'] , "<br/>";
+        echo $row['description'] , "<br/>";
+    }
+
+    
+    $user = mysqli_query($con,"select * from user");
+
+    while($r = mysqli_fetch_array($user)){
+        echo $r['user_id'] , "<br/>";
+        echo $r['name'] , "<br/>";
+        echo $r['email'] , "<br/>";
+        echo $r['password'] , "<br/>";
+
+    }
+
+
+
+    $post = mysqli_query($con,"select * from post INNER JOIN category ON post.category = category.cat_id
+    JOIN user
+  ON post.author = user.user_id;");
+    while($p = mysqli_fetch_array($post)){
+        echo $p['post_id'] , "<br/>";
+        echo $p['content'] , "<br/>";
+        echo $p['title'] , "<br/>";
+        echo $p['name'] , "<br/>";
+
+    }
+?>
---
+++ navbar.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+</head>
+<body>
+    <!-- Navbar Code Starts -->
+
+<nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+    <div class="container">
+        <a href="index.html" class="navbar-brand">JND News</a>
+        <form action="" class="d-flex">
+            <input type="search" placeholder="Search items here.." size="70" class="form-control">
+            <input type="submit" value="Search" class="btn btn-success btn-sm ms-1">
+        </form>
+        <ul class="navbar-nav">
+            <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+            <li class="nav-item"><a href="" class="nav-link">Services</a></li>
+            <li class="nav-item"><a href="" class="nav-link">About</a></li>
+            <li class="nav-item"><a href="Login.php" class="nav-link">Login</a></li>
+            <li class="nav-item"><a href="signup.php" class="nav-link">Signup</a></li>
+        </ul>
+    </div>
+</nav>
+<!-- Navbar Code Ends -->
+
+
+</body>
+</html>
